a7d248b8a822960eb83308038ab8962d132510bf
wheatleyinspace <ilyaszhilisov@mail.ru>
2025-07-12 10:03:55 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/v2/app-configs/bigsurvey', (req, res) => {
+  const filePath = path.join(__dirname, 'bigsurvey.json');
+  res.sendFile(filePath);
+});
